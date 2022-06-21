173cdf0fcce0eb1c55e59a01810338e54726921f
Hostage15 <liuhaoran8111@gmail.com>
2022-06-21 22:57:05 +1000

    [MYAPP-2] Configure the basic selection.

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -7,12 +7,22 @@ import { products } from './products';
   styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
-  public products = products
+  public products = products;
+  public product = "" as any;
+  public price : any;
+  public quantity: any;
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  createBundle(): void {
-
+  addToCart(): void {
+    
+    products.forEach((p) => {
+      if(p.name == this.product) this.price = p.price
+    })
+    console.log('Product: ', this.product);
+    console.log('Quantity: ', this.quantity);
+    console.log('Price: ', this.price);
   }
 }
