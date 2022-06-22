920bb596e505bd90179278dd20370448b19f17d5
Hostage15 <liuhaoran8111@gmail.com>
2022-06-22 12:54:40 +1000

    [MYAPP-3] Calculate price and simple validation

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { products } from './products';
+import { packaging } from './packaging';
 
 @Component({
   selector: 'app-orders',
@@ -8,21 +9,71 @@ import { products } from './products';
 })
 export class OrdersComponent implements OnInit {
   public products = products;
-  public product = "" as any;
-  public price : any;
+  public packaging = packaging;
+  public product = '' as any;
+  public price: any;
+  public bundlePrice = 0 as any;
   public quantity: any;
+  public errorMsg = '' as any;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   addToCart(): void {
-    
-    products.forEach((p) => {
-      if(p.name == this.product) this.price = p.price
-    })
-    console.log('Product: ', this.product);
-    console.log('Quantity: ', this.quantity);
-    console.log('Price: ', this.price);
+    if (this.quantity > 0 && this.product != '') {
+      this.errorMsg = '';
+      products.forEach((p) => {
+        if (p.name == this.product) {
+          this.price = p.price;
+        }
+      });
+      this.calculatePrice();
+      console.log('Product: ', this.product);
+      console.log('Quantity: ', this.quantity);
+    } else this.errorMsg = 'Please enter both product & quantity';
+  }
+
+  calculatePrice(): void {
+    let productBundle = [] as any;
+    let selectedProductCode = '';
+    this.bundlePrice = 0;
+    this.products.forEach((p) => {
+      if (this.product == p.name) selectedProductCode = p.code;
+    });
+    this.packaging.forEach((packaging) => {
+      if (selectedProductCode == packaging.code) {
+        productBundle.push(packaging);
+      }
+    });
+    console.log(productBundle, 'BUNDEL');
+    switch (productBundle.length) {
+      case 2:
+        var remainder = this.quantity % 5;
+        console.log(remainder, '%5');
+        if (this.quantity >= 5) {
+          if (remainder == 0) {
+            this.bundlePrice = 20.95 * (this.quantity / 5);
+          }
+          if (remainder == 1 || remainder == 2)
+            this.bundlePrice =
+              ((this.quantity - remainder) / 5) * 20.95 + remainder * 5.95;
+          if (remainder == 3 || remainder == 4) {
+            this.bundlePrice =
+              ((this.quantity - remainder) / 5) * 20.95 +
+              14.95 +
+              (remainder - 3) * 5.95;
+              this.bundlePrice = Math.round(this.bundlePrice * 100)/100
+          }
+        }
+        //Under 5
+        break;
+      case 3:
+        var remainder = this.quantity % 8;
+        break;
+      default:
+        console.log('SOY S');
+    }
+    console.log('Bundle price: ', this.bundlePrice);
   }
 }
---
+++ src/app/orders/packaging.ts
@@ -0,0 +1,7 @@
+export const packaging = [
+  { code: 'CE', quantity: 3, price: 14.95 },
+  { code: 'CE', quantity: 5, price: 20.95 },
+  { code: 'HM', quantity: 2, price: 13.95 },
+  { code: 'HM', quantity: 5, price: 29.95 },
+  { code: 'HM', quantity: 8, price: 40.95 },
+];
